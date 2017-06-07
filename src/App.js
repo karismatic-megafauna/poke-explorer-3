8ce4d2ef4b98f631fdb4cf1eb291ca3e22f28113
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-06-07 08:56:59 -0700

    displayed cards in grid form, added margins, and centered image

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 import { Card } from 'antd';
 import pokemon from 'pokemon-metadata';
 
-import logo from './logo.svg';
 import './App.css';
 import data from './data.json';
 
-const squirtle = pokemon.squirtle;
+class PokemonCard extends Component {
+  render() {
+    return (
+      <Card title={this.props.metadata.name} extra={this.props.metadata.id} style={{ width: 200, margin: '10px' }}>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <img src={this.props.metadata.sprites.front_default} alt={this.props.metadata.name} ></img>
+        </div>
+      </Card>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div>
-        {data.pokemon_species.map((species) => {
-          // Let's turn this into our own react component!
-          const metadata = pokemon[species.name];
-          return (
-            <Card title={metadata.name} extra={metadata.id} style={{ width: 300 }}>
-              <img src={metadata.sprites.front_default}></img>
-            </Card>
-          )
-        })}
+      <div style={{ display: 'flex', flexWrap: 'wrap'}}>
+        {data.pokemon_species.map((species) =>
+          <PokemonCard key={species.name} metadata={pokemon[species.name]}/>
+        )}
       </div>
     );
   }

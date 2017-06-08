a4003c1dbdde9984b4055d7c1d3e30cca91c30c5
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-06-08 13:54:05 -0700

    Add proptypes, add search and filter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card, Input } from 'antd';
 import pokemon from 'pokemon-metadata';
+import PropTypes from 'prop-types';
 
 import './App.css';
 import data from './data.json';
+
+const Search = Input.Search;
 
 class PokemonCard extends Component {
   render() {
@@ -17,17 +20,42 @@ class PokemonCard extends Component {
   }
 }
 
+PokemonCard.propTypes = {
+  metadata: PropTypes.shape({
+    name: PropTypes.string,
+    id: PropTypes.string,
+    sprites: PropTypes.shape({
+      front_default: PropTypes.string
+    })
+  })
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {search: ''};
+  }
+
+  onTextChange = (event) => {
+    this.setState({search: event.target.value});
+  }
+
   render() {
     return (
       <div>
         <div>
-          <input></input>
+          <Search
+            placeholder="input pokemon search text"
+            style={{ width: 200 }}
+            onSearch={value => console.log(value)}
+            onChange={this.onTextChange}
+            />
         </div>
         <div style={{ display: 'flex', flexWrap: 'wrap'}}>
-          {data.pokemon_species.map((species) =>
-            <PokemonCard key={species.name} metadata={pokemon[species.name]}/>
-          )}
+          {data.pokemon_species
+            .filter((species) => species.name.includes(this.state.search))
+            .map((species) => <PokemonCard key={species.name} metadata={pokemon[species.name]}/>)
+          }
         </div>
       </div>
     );
